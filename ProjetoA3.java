d2595be0a6e1881c82407a3cb114cd6db2f54d06
Cauã Pincelli Cacchero <cauapincacchero@gmail.com>
2025-04-22 19:53:40 -0300

    feat: tela de login

---
+++ ProjetoA3.java
@@ -0,0 +1,101 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JPasswordField;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+public class ProjetoA3 extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JPasswordField passwordField;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ProjetoA3 frame = new ProjetoA3();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ProjetoA3() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\825163387\\Downloads\\2b83c11f-b1de-4a88-ac96-5120c8186929.jfif"));
+		setForeground(new Color(255, 255, 255));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1360, 898);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\825163387\\Downloads\\WhatsApp Image 2025-04-17 at 16.26.48.jpeg"));
+		lblNewLabel.setBounds(388, 0, 597, 321);
+		contentPane.add(lblNewLabel);
+		
+		passwordField = new JPasswordField();
+		passwordField.setFont(new Font("Arial", Font.PLAIN, 18));
+		passwordField.setBounds(424, 590, 442, 54);
+		contentPane.add(passwordField);
+		
+		JLabel lblNewLabel_1 = new JLabel("Entre na sua Conta");
+		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 28));
+		lblNewLabel_1.setForeground(new Color(255, 128, 0));
+		lblNewLabel_1.setBounds(496, 285, 358, 38);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Insira seu e-mail e sua senha para acessar sua conta!");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_2.setBounds(467, 332, 364, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("EMAIL");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_3.setBounds(424, 396, 46, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		JButton btnNewButton = new JButton("Entrar");
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.setBackground(new Color(0, 0, 0));
+		btnNewButton.setBounds(424, 677, 443, 38);
+		contentPane.add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Arial", Font.PLAIN, 18));
+		textField.setBounds(424, 442, 442, 54);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_3_1 = new JLabel("SENHA");
+		lblNewLabel_3_1.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_3_1.setBounds(424, 545, 46, 14);
+		contentPane.add(lblNewLabel_3_1);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("Esqueceu sua senha?");
+		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel_2_1.setBounds(424, 744, 442, 38);
+		contentPane.add(lblNewLabel_2_1);
+	}
+}

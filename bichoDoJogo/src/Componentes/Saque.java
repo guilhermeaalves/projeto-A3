1294dd2a654e6ee8229490e7ed5dd145a09c8460
MiguelBurgatti <mihburgatti@gmail.com>
2025-04-23 20:13:54 -0300

    Feat: Saque e Deposito

--- bichoDoJogo/src/Componentes/Saque.java
+++ bichoDoJogo/src/Componentes/Saque.java
@@ -8,12 +8,12 @@ package Componentes;
  *
  * @author 825128186
  */
-public class Deposito extends javax.swing.JFrame {
+public class Saque extends javax.swing.JFrame {
 
     /**
      * Creates new form TelaInicial
      */
-    public Deposito() {
+    public Saque() {
         initComponents();
     }
 
@@ -247,14 +247,22 @@ public class Deposito extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Deposito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Saque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Deposito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Saque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Deposito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Saque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Deposito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Saque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
@@ -267,7 +275,7 @@ public class Deposito extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Deposito().setVisible(true);
+                new Saque().setVisible(true);
             }
         });
     }

d9a2526df43854ef7b07f55afe596af74d77a28c
Guilherme Alves <contatoguilhermealv@gmail.com>
2025-05-23 19:39:23 -0300

    rebase

---
+++ app/src/database/Conexao.java
@@ -0,0 +1,30 @@
+package database;
+import java.sql.*;
+
+public class Conexao {
+
+    private static final String url = "jdbc:mysql://localhost:3306/jogoDoBicho";
+    private static final String user = "root";
+    private static final String password = "0584";
+
+    private static Connection conn;
+
+    public static Connection getConexao() {
+        try {
+
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Conectado com sucesso!");
+            return conn;
+        } catch (SQLException e) {
+            System.err.println("Erro ao conectar!");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        getConexao();
+    }
+
+}
